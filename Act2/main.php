08e606a748c8107c6058b171d72fd5d630ba345f
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-09-28 19:17:14 +0200

    Act2 full

--- Act2/main.php
+++ Act2/main.php
@@ -6,8 +6,8 @@
         <form method="post">
             <p>Nombre: <input for="nombre" name="nombre"></p>
             <p>Modulo: </p>
-            <p><input type="radio" value="Desarrollo Web en Entorno Servidor" name="modulo">Desarrollo Web en Entorno Servidor</p>
-            <p><input type="radio" value="Desarrollo Web en Entorno Cliente" name="modulo">Desarrollo Web en Entorno Cliente</p>
+            <p><input type="checkbox" value="Desarrollo Web en Entorno Servidor" name="modulo1">Desarrollo Web en Entorno Servidor</p>
+            <p><input type="checkbox" value="Desarrollo Web en Entorno Cliente" name="modulo2">Desarrollo Web en Entorno Cliente</p>
             <input type="submit" value="Enviar">
         </form>
     </body>
@@ -15,8 +15,12 @@
 
     <?php 
     
-    if( (!empty($_REQUEST["nombre"])) and (!empty($_REQUEST["modulo"])) ){
-        echo "El nombre es ", $_REQUEST["nombre"]," y está en el módulo ", $_REQUEST["modulo"];
+    if( (!empty($_REQUEST["nombre"])) and (!empty($_REQUEST["modulo1"])) and (empty($_REQUEST["modulo2"])) ){
+        echo "El nombre es ", $_REQUEST["nombre"], " y está en el módulo de ", $_REQUEST["modulo1"];
+    }elseif ( (!empty($_REQUEST["nombre"])) and (empty($_REQUEST["modulo1"])) and (!empty($_REQUEST["modulo2"])) ) {
+        echo "El nombre es ", $_REQUEST["nombre"], " y está en el módulo de ", $_REQUEST["modulo2"];
+    }elseif ( (!empty($_REQUEST["nombre"])) and (!empty($_REQUEST["modulo1"])) and (!empty($_REQUEST["modulo2"])) ) {
+        echo "El nombre es ", $_REQUEST["nombre"], " y está en los módulos de ", $_REQUEST["modulo1"], " y ", $_REQUEST["modulo2"];
     }
 
     ?>
