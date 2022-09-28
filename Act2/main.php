37fa8fb736eb7e9e5a07ebf1491bf137f8771d61
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-09-28 19:00:17 +0200

    Correcto Act2

---
+++ Act2/main.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>GGS</title>
+    </head>
+    <body>
+        <form method="post">
+            <p>Nombre: <input for="nombre" name="nombre"></p>
+            <p>Modulo: </p>
+            <p><input type="radio" value="Desarrollo Web en Entorno Servidor" name="modulo">Desarrollo Web en Entorno Servidor</p>
+            <p><input type="radio" value="Desarrollo Web en Entorno Cliente" name="modulo">Desarrollo Web en Entorno Cliente</p>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+
+    <?php 
+    
+    if((!empty($_REQUEST["nombre"]))and(!empty($_REQUEST["modulo"]))){
+        echo "El nombre es ", $_REQUEST["nombre"]," y está en el módulo ", $_REQUEST["modulo"];
+    }
+    
+    ?>
+
+</html>
