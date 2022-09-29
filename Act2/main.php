ad3a954bc39a5d10c1232cfe3e32b6be70675365
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-09-29 17:48:44 +0200

    Act2 sin funciones

--- Act2/main.php
+++ Act2/main.php
@@ -6,31 +6,47 @@
         <form method="post">
             <p>Nombre: <input for="nombre" name="nombre"></p>
             <p>Modulo: </p>
-            <p><input type="checkbox" value="Desarrollo Web en Entorno Servidor" name="modulo"[]>Desarrollo Web en Entorno Servidor</p>
-            <p><input type="checkbox" value="Desarrollo Web en Entorno Cliente" name="modulo"[]>Desarrollo Web en Entorno Cliente</p>
+            <p><input type="checkbox" value="Desarrollo Web en Entorno Servidor" name="modulo[]">Desarrollo Web en Entorno Servidor</p>
+            <p><input type="checkbox" value="Desarrollo Web en Entorno Cliente" name="modulo[]">Desarrollo Web en Entorno Cliente</p>
             <input type="submit" value="Enviar">
         </form>
     </body>
 </html>
 
     <?php
+    static $checkpoint = 0;
 
-    if((!empty($_REQUEST["nombre"]))){
-        echo "Nombre: ", $_REQUEST["nombre"];
+    if($checkpoint == 0){
+        $checkpoint = 1;
+    }else{
+        if((empty($_REQUEST["nombre"]))){
+            echo "No se ha especificado un nombre";
+        }else{
+            echo "Nombre: ", $_REQUEST["nombre"];
+        }
     }
+
+        
+
+
+
+
     
     ?>
 
     <br/>
 
     <?php
-        $modulos[] = array($_REQUEST["modulo"]);
-    if( !empty($modulos) ){
 
-       foreach ($modulos  as $nombre) {
-        echo "Modulo: ", $nombre;
+    $modulos = array("Desarrollo Web en Entorno Servidor", "Desarrollo Web en Entorno Cliente");
+
+    foreach ($modulo as $modulos) {
+        if(!empty($nombre)){
+            echo "Modulo: ", $modulo;
+        }
     }
-     
-    }
+
+
+
 
 ?>
