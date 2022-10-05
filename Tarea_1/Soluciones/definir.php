e7cb5ebeacf7f6e7c13af45e0d7589722a8026d1
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-10-05 19:23:01 +0200

    Arrays

--- Tarea_1/Soluciones/definir.php
+++ Tarea_1/Soluciones/definir.php
@@ -10,8 +10,8 @@
      * Realizar un fichero php donde se definan tres arrays de 9x9 elementos enteros,
      * para almacenar los siguientes 3 sudokus iniciales de tres niveles de dificultad distintos:
      */
-        
-        $facil = array(
+        $sudoku = array (
+        array(
             array('.','.',1,9,4,8,5,'.','.'),
             array('.','.',3,7,'.',6,1,'.','.'),
             array('.',5,'.','.','.','.','.',7,'.'),
@@ -21,9 +21,9 @@
             array('.',6,'.','.','.','.','.',1,'.'),
             array('.','.',7,1,'.',9,4,'.','.'),
             array('.','.',5,8,6,3,7,'.','.')
-        );
+        ),
     
-    $medio = array(
+        array(
             array('.','.','.','.',8,4,'.','.',2),
             array(2,'.','.','.','.','.',5,'.','.'),
             array('.',3,'.',1,'.','.','.',4,'.'),
@@ -33,9 +33,9 @@
             array('.',6,'.','.','.',5,'.',7,'.'),
             array('.','.',8,'.','.','.','.','.',6),
             array(9,'.','.',4,1,'.','.','.','.')
-        );
+    ),
 
-        $dificil = array(
+        array(
             array(6,2,'.','.','.',4,'.',7,'.'),
             array(5,'.',3,'.',9,'.','.','.','.'),
             array(8,'.','.','.',6,'.','.',3,'.'),
@@ -45,8 +45,8 @@
             array('.',5,'.',3,'.','.','.','.',2),
             array('.','.','.',7,'.','.',6,'.',3),
             array('.',7,'.',2,'.','.','.',1,8)
-        );
-
+        )
+    );
     ?>
 </body>
 </html>
---
+++ Tarea_1/Soluciones/generar.php
@@ -0,0 +1,16 @@
+<! DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+</head>
+<body>
+<?php
+require('definir.php');
+echo "<table>";
+    for($i=0;$i<$sudoku.count;$i++){
+       
+    }
+echo "</table>";
+?>
+</body>
+</html>
