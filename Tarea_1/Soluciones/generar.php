89e0302e3d174bc5780de35a49e814b4e60ea5d4
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-10-06 12:19:20 +0200

    Buscado de internet pero con el facil/medio/dificil añadido

--- Tarea_1/Soluciones/generar.php
+++ Tarea_1/Soluciones/generar.php
@@ -6,7 +6,24 @@
 <body>
 <?php
 require('definir.php');
+$counter = 1;
+
 foreach ($sudoku as $tipo) {
+
+    switch ($counter) {
+        case '1':
+            echo "Fácil";
+            break;
+
+        case '2':
+            echo "Medio";
+            break;
+        
+        case '3':
+            echo "Difícil";
+            break;
+    }
+
     echo "<table>";
     foreach ($tipo as $fila) {
         echo '<tr>';
@@ -16,6 +33,7 @@ foreach ($sudoku as $tipo) {
         echo '</tr>';
     }
     echo "</table>";
+    $counter++;
 }
 
 ?>
