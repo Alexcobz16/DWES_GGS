18acbe4faf7e76795f6cb9bc935c185b05ecae76
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-10-07 20:15:48 +0200

    Selector a medio crear (parte facil la vd)

---
+++ Tarea_1/Soluciones/index1.php
@@ -0,0 +1,17 @@
+<?php include('generar.php'); 
+      echo "<br/>";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Selector de dificultad</title>
+    </head>
+    <body>
+        <h1>Elige dificultad</h1>
+        <br/>
+        <form>
+            
+        </form>
+    </body>
+</html>
