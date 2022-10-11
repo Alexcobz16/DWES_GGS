00436c6c247b99a52669eb68db3605d340874cff
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-10-11 17:33:35 +0200

    Arreglao

--- Tarea_1/Soluciones/index1.php
+++ Tarea_1/Soluciones/index1.php
@@ -10,13 +10,9 @@
     <body>
         <h1>Elige dificultad</h1>
         <br/>
-        <form method="post">  
+        <form method="post" action="index2.php">  
             <p><input type="radio" value="1" name="dificultad">Fácil <input type="radio" value="2" name="dificultad">Medio <input type="radio" value="3" name="dificultad">Difícil</p>
-            <a href="index2.php"><input type="submit" value="Jugar" href="index2.php"></a>
+           <input type="submit" value="Jugar">
         </form>
     </body>
 </html>
-
-<?php
-    $dificultad = $_POST['dificultad'];
-?>
--- Tarea_1/Soluciones/partida.php
+++ Tarea_1/Soluciones/partida.php
@@ -1,15 +1,58 @@
-<?php include_once('index1.php');
+<?php require('definir.php');
+
+$dificultad = $_POST['dificultad'];
 
 switch ($dificultad) {
     case '1':
-        echo "<p>Imprime sudoku facil</p>";
+        echo "<table>";
+
+        for($i=0;$i<$sudoku[$dificultad-1].count;$i++){
+            echo '<tr>';
+
+            for($j=0;$j<$sudoku[$dificultad-1][$i].count;$j++){
+                if($sudoku[$dificultad-1][$i][$j] == '.'){
+                    echo "<td class='huecos'>",$sudoku[$dificultad-1][$i][$j],"</td>";
+                }else{
+                    echo "<td class='pistas'>",$sudoku[$dificultad-1][$i][$j],"</td>";
+                }
+            }
+            echo '</tr>';
+        }
+        echo "</table>";    
         break;
     case '2':
-        echo "<p>Imprime sudoku medio</p>";
+        echo "<table>";
+
+        for($i=0;$i<$sudoku[$dificultad-1].count;$i++){
+            echo '<tr>';
+
+            for($j=0;$j<$sudoku[$dificultad-1][$i].count;$j++){
+                if($sudoku[$dificultad-1][$i][$j] == '.'){
+                    echo "<td class='huecos'>",$sudoku[$dificultad-1][$i][$j],"</td>";
+                }else{
+                    echo "<td class='pistas'>",$sudoku[$dificultad-1][$i][$j],"</td>";
+                }
+            }
+            echo '</tr>';
+        }
+        echo "</table>";  
         break;
     case '3':
-        echo "<p>Imprime sudoku dificil</p>";
-        break;    
-    }
+        echo "<table>";
 
+        for($i=0;$i<$sudoku[$dificultad-1].count;$i++){
+            echo '<tr>';
+
+            for($j=0;$j<$sudoku[$dificultad-1][$i].count;$j++){
+                if($sudoku[$dificultad-1][$i][$j] == '.'){
+                    echo "<td class='huecos'>",$sudoku[$dificultad-1][$i][$j],"</td>";
+                }else{
+                    echo "<td class='pistas'>",$sudoku[$dificultad-1][$i][$j],"</td>";
+                }
+            }
+            echo '</tr>';
+        }
+        echo "</table>";  
+        break; 
+}
 ?>
