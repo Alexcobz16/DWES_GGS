286b87eefe4401c3a894b9394fed86d18d377fe7
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-10-14 20:22:56 +0200

    depts

--- GGS2/depts.php
+++ GGS2/depts.php
@@ -1,5 +1,7 @@
 
-<?php $conexion = new mysqli('localhost', 'usuario', 'contraseña', 'base_de_datos');
+<?php 
+
+$conexion = new mysqli('localhost', 'usuario', 'contraseña', 'base_de_datos');
 
 print "<p>"; print $conexion->server_info; print "</p>";
 
