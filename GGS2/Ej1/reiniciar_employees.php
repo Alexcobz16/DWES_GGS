523c3702d03ba05e3a66b0d00372738312dfa53e
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-11-11 00:11:56 +0100

    no se si funcionara

--- GGS2/Ej1/reiniciar_employees.php
+++ GGS2/Ej1/reiniciar_employees.php
@@ -15,14 +15,47 @@ function resetear($plataforma){ ?>
 if(($plataforma == "win") && (isset($_POST['login'])) && (isset($_POST['user']) && !empty($_POST['user']))){
     if(isset($_POST['psswd']) && !empty($_POST['psswd'])){
         exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/employees.sql");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_departments.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_employees.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_dept_emp.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_dept_manager.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_titles.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_salaries1.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_salaries2.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " -p". $_POST['psswd'] ." < database/load_salaries3.dump");
     }else{
         exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/employees.sql");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_departments.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_employees.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_dept_emp.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_dept_manager.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_titles.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_salaries1.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_salaries2.dump");
+        exec("C:\xampp\mysql\bin\mysql.exe -u ". $_POST['user'] . " < database/load_salaries3.dump");
     }
     }else{
         if(isset($_POST['psswd']) && !empty($_POST['psswd'])){
-           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < employees.sql");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/employees.sql");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_departments.dump");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_employees.dump");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_dept_emp.dump");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_dept_manager.dump");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_titles.dump");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_salaries1.dump");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_salaries2.dump");
+           exec("mysql --user=". $_POST['user'] ." --password=". $_POST['psswd'] ." < database/load_salaries3.dump");
         }else{
-            exec("mysql --user=". $_POST['user'] ." < employees.sql");
+            exec("mysql --user=". $_POST['user'] ." < database/employees.sql");
+            exec("mysql --user=". $_POST['user'] ." < database/load_departments.dump");
+            exec("mysql --user=". $_POST['user'] ." < database/load_employees.dump");
+            exec("mysql --user=". $_POST['user'] ." < database/load_dept_emp.dump");
+            exec("mysql --user=". $_POST['user'] ." < database/load_dept_manager.dump");
+            exec("mysql --user=". $_POST['user'] ." < database/load_titles.dump");
+            exec("mysql --user=". $_POST['user'] ." < database/load_salaries1.dump");
+            exec("mysql --user=". $_POST['user'] ." < database/load_salaries2.dump");
+            exec("mysql --user=". $_POST['user'] ." < database/load_salaries3.dump");
+
         }    
     }
 }
