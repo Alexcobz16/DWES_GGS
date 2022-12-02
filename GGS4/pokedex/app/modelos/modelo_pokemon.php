61ad0cb44feccc77e24e65e6c5ddea95bc32a460
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-12-02 22:08:40 +0100

    la practica 4 (cague+)

--- GGS4/pokedex/app/modelos/modelo_pokemon.php
+++ GGS4/pokedex/app/modelos/modelo_pokemon.php
@@ -1,8 +1,12 @@
 <?php
-try{
-    $conexion = new PDO('mysql:dbname=pokemon;host=localhost;','root','');
-    echo "La base funciona";
-}catch(Exception $e){
-    echo "La base da petazo en la conexion";
-}
 
+class Pokemon{
+    public function conectar(){
+        try{
+            $conexion = new PDO('mysql:dbname=pokemon;host=localhost;','root','');
+            echo "La base funciona";
+        }catch(Exception $e){
+            echo "La base da petazo en la conexion";
+        }
+    }
+};
