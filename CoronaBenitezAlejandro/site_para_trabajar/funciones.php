52991676ec4118d9c3b22ac11e12b874368e8ede
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2023-01-09 18:22:24 +0100

    mucha mierda 2022

--- CoronaBenitezAlejandro/site_para_trabajar/funciones.php
+++ CoronaBenitezAlejandro/site_para_trabajar/funciones.php
@@ -4,9 +4,62 @@
  * Ten presente los ámbitos de las variables y los modificadores que puedes utilizar para cambiarlo
  */
 function getBasketMarkup(){
+  if(isset($_POST['sumar'])){
+    $nombre = array_keys($_POST['sumar']);
+    $posicion = 0;
+    $existe = false;
+    for($i=0;$i<count($_SESSION['cesta']['productos']);$i++){
+      $articulos = array_keys($_SESSION['cesta']['productos'][$i]);
+      foreach($articulos as $articulo){
+        if($articulo == $nombre[0]){
+          $existe = true;
+          $posicion = $i;
+          break;
+        }
+      }
+    }
+    $_SESSION['cesta']['productos'][$posicion][$nombre[0]]['cantidad']++;
+  }else if(isset($_POST['restar'])){
+    $nombre = array_keys($_POST['restar']);
+    $posicion = 0;
+    $existe = false;
+    for($i=0;$i<count($_SESSION['cesta']['productos']);$i++){
+      $articulos = array_keys($_SESSION['cesta']['productos'][$i]);
+      foreach($articulos as $articulo){
+        if($articulo == $nombre[0]){
+          $existe = true;
+          $posicion = $i;
+          break;
+        }
+      }
+    }
+    $_SESSION['cesta']['productos'][$posicion][$nombre[0]]['cantidad']--;
+  }
+
   //Ejemplo del HTML generado: ( no tiene por qué coincidir exactamente con el presente en la plantilla HTML )
   //Hay que incluir form
-  $basket_markup = '';
+  $total = 0;
+  foreach($_SESSION['cesta']['productos'] as $producto){
+    foreach($producto as $contenido){
+      $total += $contenido['cantidad'] * $contenido['precio'];
+    }
+  }
+  $basket_markup = '<form method="post"><p><strong>Número de items:</strong>'.count($_SESSION['cesta']['productos']).'</p>
+  <p><strong>Precio total:</strong>'.$total.'</p>';
+  foreach($_SESSION['cesta']['productos'] as $producto){
+    foreach($producto as $contenido){
+      $total += $contenido['cantidad'] * $contenido['precio'];
+      $basket_markup .= '<hr>
+      <div class="cItemContainer">
+        <div class="cFoto"><img src="'.$contenido["img_miniatura"].'"></div>
+        <div class="cNombreProducto"><h3>'.$contenido["nombre"].'</h3></div>
+        <input type="submit" value="-" name="restar['.$contenido["nombre"].']">'.$contenido['cantidad'].'<input type="submit" value="+" name="sumar['.$contenido["nombre"].']">
+        <strong>Precio:</strong> '.$contenido["precio"].'
+      </div>
+  ';
+    }
+  }
+  $basket_markup .= '</form>';
 /*  
  <p><strong>Número de items:</strong> 2</p>
       <p><strong>Precio total:</strong> $800</p>
@@ -28,21 +81,23 @@ function getBasketMarkup(){
     return $basket_markup;
   }
 
+
 /** La siguiente función debe generar el código HTML de los productos, con sus botones de 'add to cart' cesta
  * Ten presente los ámbitos de las variables y los modificadores que puedes utilizar para cambiarlo
  */
   function getProductosMarkup($productos){
    //Ejemplo del HTML generado: ( no tiene por qué coincidir exactamente con el presente en la plantilla HTML )
    //Hay que incluir form
-   $productos_markup = '';
+   $productos_markup = '<form method="post">';
 
   foreach($productos as $producto){
-    $productos_markup .= '<div class="cProductoContainer"><img src="<?php echo $producto["img_url"]; ?>" alt="" width="270" height="280">
-        <input type="submit" value="Incluir en cesta" name="add">
-        <h4><?php echo $producto["nombre"]; ?></h4>
-        <p><strong>$ <?php echo $producto["precio"]; ?></strong></p>
+    $productos_markup .= '<div class="cProductoContainer"><img src="'.$producto["img_url"].'" alt="" width="270" height="280">
+        <input type="submit" value="Incluir en cesta" name="add['.$producto["nombre"].']">
+        <h4>'.$producto["nombre"].'</h4>
+        <p><strong>$ '.$producto["precio"].'</strong></p>
       </div>';
     }
+    $productos_markup .= '</form>';
 
     /*<!-- Producto-->          
       <div class="cProductoContainer"><img src="./images/product-5-270x280.png" alt="" width="270" height="280">
--- CoronaBenitezAlejandro/site_para_trabajar/index.php
+++ CoronaBenitezAlejandro/site_para_trabajar/index.php
@@ -1,10 +1,9 @@
 <?php
   session_start();
   
-  ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
 
   require_once('./productos.php');
   require_once('./funciones.php');
@@ -16,15 +15,44 @@ session_start();
 
   //Aquí puedes gestionar los post. Hay varias funcionalidades en la página (dos formularios): incluir en cesta, subir un determinado producto en una unidad y bajar un determinado producto de la cesta en una unidad. La manera de sacar los productos de la cesta es poner a 0 el número de unidades que hay en la cesta
   
-  $_SESSION['cesta'] = array(
-    'productos' => array(
-
-    )
-  );
+  if(!isset($_SESSION['cesta'])){
+    $_SESSION['cesta'] = array(
+      'productos' => array(
+  
+      )
+    );  
+  }
 
   if(isset($_POST['add'])){
-    foreach(){
-      
+    $nombre = array_keys($_POST['add']);
+    if(count($_SESSION['cesta']['productos'])==0){
+      foreach($productos as $producto){
+        if($producto['nombre'] == $nombre[0]){
+          array_push($_SESSION['cesta']['productos'], array($nombre[0] => array('precio' => $producto['precio'], 'cantidad' => 1, 'nombre' => $nombre[0], 'img_miniatura' => $producto['img_miniatura'])));
+        }
+      }
+    }else{
+      $posicion = 0;
+      $existe = false;
+      for($i=0;$i<count($_SESSION['cesta']['productos']);$i++){
+        $articulos = array_keys($_SESSION['cesta']['productos'][$i]);
+        foreach($articulos as $articulo){
+          if($articulo == $nombre[0]){
+            $existe = true;
+            $posicion = $i;
+            break;
+          }
+        }
+      }
+      if($existe){
+        $_SESSION['cesta']['productos'][$posicion][$nombre[0]]['cantidad']++;
+      }else{
+        foreach($productos as $producto){
+          if($producto['nombre'] == $nombre[0]){
+            array_push($_SESSION['cesta']['productos'], array($nombre[0] => array('precio' => $producto['precio'], 'cantidad' => 1, 'nombre' => $nombre[0], 'img_miniatura' => $producto['img_miniatura'])));
+          }
+        }
+      }
     }
   }
   
